39b3040baf9d2597203a8d648548faa5137f50ba
Manan Garg <manangarg1929@gmail.com>
2023-09-25 09:57:42 +0530

    Update Coalnltk.py

--- Coal Backend/Coalnltk.py
+++ Coal Backend/Coalnltk.py
@@ -1,1 +1,25 @@
+import numpy as num
+import nltk
+#nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
 
+def token(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return PorterStemmer().stem(word.lower())
+
+
+def bag_of_words(TokenizeSentence, AllWords):
+    sentence_words = [stem(word) for word in TokenizeSentence]
+    # initialize bag with 0 for each word
+    bag = num.zeros(len(AllWords), dtype=num.float32)
+    for i, w in enumerate(AllWords):
+        if w in sentence_words: 
+            bag[i] = 1
+    return bag
+
+#words=["Working","Works","Worked"]
+#stemmed_words=[stem(w) for w in words]
+#print("The Stemmed Words are :",stemmed_words)
